244e9fd8540a1a6900a272ef7683e13e72b295fb
Alejae1998 <alejagr1998@gmail.com>
2023-01-12 14:42:12 -0800

    working on app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [head, setHead] = useState('');
+  const [middle, setMiddle] = useState('');
+  const [bottom, setBottom] = useState('');
+  const [input, setInput] = useState('');
+  const [catchphrase, setCatchphrase] = useState([]);
+  const [headCount, setHeadCount] = useState(0);
+  const [middleCount, setMiddleCount] = useState(0);
+  const [bottomCount, setBottomCount] = useState(0);
 }
 
 export default App;
