d4c0d76ad680331ae7d2e8b0fb1051cbe3436dc6
Alejae1998 <alejagr1998@gmail.com>
2023-01-12 16:00:22 -0800

    adding my props to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-
+import Display from './components/Display';
+import Cathphrase from './components/Catchphrase';
+import Controls from './components/Controls';
+import bg from './bg.jpeg';
 function App() {
   const [head, setHead] = useState('');
   const [middle, setMiddle] = useState('');
@@ -11,6 +14,46 @@ function App() {
   const [headCount, setHeadCount] = useState(0);
   const [middleCount, setMiddleCount] = useState(0);
   const [bottomCount, setBottomCount] = useState(0);
+
+  const handleClick = () => {
+    if (!input) return;
+    setCatchphrase((prevState) => {
+      return [...prevState, input];
+    });
+    setInput('');
+  };
+
+  const handleChange = (type, value) => {
+    if (type === 'head') {
+      setHead(value);
+      setHeadCount(headCount + 1);
+    }
+    if (type === 'middle') {
+      setMiddle(value);
+      setMiddleCount(middleCount + 1);
+    }
+    if (bottom === 'bottom') {
+      setBottom(value);
+      setBottomCount(bottomCount + 1);
+    }
+  };
+
+  return (
+    <div className="App" style={{ backgroundImage: `url(${bg})` }}>
+      <main>
+        <h1>Character Designer</h1>
+        <section className="container">
+          <div className="left">
+            <Controls />
+            <Cathphrase />
+          </div>
+          <div className="right">
+            <Display />
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
